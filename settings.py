9e3684a7e1cfca390081478a9c6b02f934249e5e
zaheer mv <muhdzaheermv@gmail.com>
2025-03-15 20:58:09 +0530

    HOST

--- settings.py
+++ settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
