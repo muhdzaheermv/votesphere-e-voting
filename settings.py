bf871686fe0565db119658bc5f9332e39c7d0893
zaheer mv <muhdzaheermv@gmail.com>
2025-03-15 21:04:17 +0530

    HOSTing

--- settings.py
+++ settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "True"
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vote'
 ]
 
 MIDDLEWARE = [
